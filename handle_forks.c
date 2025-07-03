81320d4ef063afbc37834c823fa2338f18da4e65
frnki <frnki@frnki.dev>
2025-07-03 02:11:05 +0200

    refactor: smarter lock and unlock management

--- handle_forks.c
+++ handle_forks.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   handle_forks.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: .frnki   <frnki@42.fr>                     +#+  +:+       +#+        */
+/*   By: .frnki   <frnki@frnki.dev>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 16:20:42 by .frnki            #+#    #+#             */
 /*   Updated: 2025/02/25 16:42:42 by .frnki           ###   ########.fr       */
@@ -17,16 +17,15 @@ void	pick_left_first(t_philo *philo)
 	long	timestamp;
 
 	pthread_mutex_lock(philo->fork_left);
-	pthread_mutex_lock(&philo->table->msg_lock);
-	timestamp = check_time() - philo->table->t_start;
-	if (philo->table->all_alive)
-		printf("%li %i has taken a fork (left)\n", timestamp, philo->num);
-	pthread_mutex_unlock(&philo->table->msg_lock);
 	pthread_mutex_lock(philo->fork_right);
 	pthread_mutex_lock(&philo->table->msg_lock);
-	timestamp = check_time() - philo->table->t_start;
 	if (philo->table->all_alive)
+	{
+		timestamp = check_time() - philo->table->t_start;
+		printf("%li %i has taken a fork (left)\n", timestamp, philo->num);
+		timestamp = check_time() - philo->table->t_start;
 		printf("%li %i has taken a fork (right)\n", timestamp, philo->num);
+	}
 	pthread_mutex_unlock(&philo->table->msg_lock);
 }
 
@@ -36,19 +35,12 @@ void	pick_right_first(t_philo *philo)
 	long	timestamp;
 
 	pthread_mutex_lock(philo->fork_right);
+	pthread_mutex_lock(philo->fork_left);
+	pthread_mutex_lock(&philo->table->msg_lock);
 	if (philo->table->all_alive)
 	{
-		pthread_mutex_lock(&philo->table->msg_lock);
 		timestamp = check_time() - philo->table->t_start;
 		printf("%li %i has taken a fork (right)\n", timestamp, philo->num);
-		pthread_mutex_unlock(&philo->table->msg_lock);
-	}
-	else
-		return ;
-	pthread_mutex_lock(philo->fork_left);
-	if (philo->table->all_alive)
-	{
-		pthread_mutex_lock(&philo->table->msg_lock);
 		timestamp = check_time() - philo->table->t_start;
 		printf("%li %i has taken a fork (left)\n", timestamp, philo->num);
 	}
@@ -70,15 +62,16 @@ void	put_down_forks(t_philo *philo)
 	long	timestamp;
 
 	pthread_mutex_unlock(philo->fork_left);
-	pthread_mutex_lock(&philo->table->msg_lock);
-	timestamp = check_time() - philo->table->t_start;
-	if (philo->table->all_alive)
-		printf("%li %i put a fork down (left)\n", timestamp, philo->num);
-	pthread_mutex_unlock(&philo->table->msg_lock);
 	pthread_mutex_unlock(philo->fork_right);
 	pthread_mutex_lock(&philo->table->msg_lock);
-	timestamp = check_time() - philo->table->t_start;
 	if (philo->table->all_alive)
-		printf("%li %i put a fork down (right)\n", timestamp, philo->num);
+	{
+		timestamp = check_time() - philo->table->t_start;
+		printf("%li %i put a fork down (left)\n",
+			timestamp, philo->num);
+		timestamp = check_time() - philo->table->t_start;
+		printf("%li %i put a fork down (right)\n",
+			timestamp, philo->num);
+	}
 	pthread_mutex_unlock(&philo->table->msg_lock);
 }
